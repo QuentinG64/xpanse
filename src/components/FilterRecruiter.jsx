e8404ef6993fd863f2f5e888a10f7e7eef500336
Quentin GUERANDEL <quentin.guerandel@gmail.com>
2022-04-11 15:09:49 +0200

    updated filter recruiterPage

--- src/components/FilterRecruiter.jsx
+++ src/components/FilterRecruiter.jsx
@@ -3,11 +3,13 @@ import Checkbox from "./Checkbox";
 
 const FilterRecruiter = () => {
   return (
-    <div className="flex flex-col border-2">
+    <div className="flex flex-col border-2 rounded-xl h-64">
       <h1 className="font-title text-main-2 text-lg">
         SELECT YOUR NEXT PARTNER
       </h1>
-      <h2 className="font-title text-main-2 text-sm">PARTNER'S STATUS</h2>
+      <h2 className="font-title text-main-2 text-sm">
+        FILTER BY PARTNER'S STATUS
+      </h2>
       <div className="text-main-2 font-primary text-sm">
         <Checkbox title="Retired" />
         <Checkbox title="Active" />
