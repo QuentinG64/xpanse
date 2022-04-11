85fd8775b01934cd8bed947036312048c652c3d8
Nawel Benaicha <nawelbenaicha@MBP-de-Nawel.lan>
2022-04-11 16:31:53 +0200

    Nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,20 @@
 const NavBar = () => {
   return (
-    <nav className="fixed z-10 w-full h-10">
-      <ul className="flex flex-row justify-around p-5 m-0 text-xl list-none cursor-pointer bg-slate-600 text-slate-200">
-        <li className=" hover:text-slate-800">Dinothèque</li>
-        <li className=" hover:text-slate-800">Combat de dinos</li>
-        <li className=" hover:text-slate-800">Boutique</li>
+    <nav className="font-title flex justify-between bg-none">
+      <p className="flex text-2xl ml-7">Xpanse</p>
+      <ul className="mr-10 space-x-4 text-black flex justify-around bg-none">
+        <li className="pr-20 hover:underline decoration-solid decoration-primary-1">
+          FREELANCE
+        </li>
+        <li className="pr-20 hover:underline decoration-solid decoration-primary-1">
+          RECRUITER
+        </li>
+        <li className="pr-20 hover:underline decoration-solid decoration-primary-1">
+          GOLDEN TICKET
+        </li>
+        <li className=" hover:underline decoration-solid decoration-primary-1">
+          ABOUT US
+        </li>
       </ul>
     </nav>
   );
