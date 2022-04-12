060008a644ce5ad67f6b821ee189f313ced4c684
AntsoTech <antso-rakotoson@hotmail.fr>
2022-04-12 15:12:36 +0200

    run lint and prettier on button,countdown and goldenticket

--- src/components/GoldenTicket.jsx
+++ src/components/GoldenTicket.jsx
@@ -1,16 +1,24 @@
 import Button from "./Button";
-import Countdown from "./Countdown.jsx"
+import Countdown from "./Countdown.jsx";
 
 const GoldenTicket = () => {
-    return (
-            <div className="h-screen bg-cover bg-[url('../assets/images/goldenTicketBg.png')] text-main-2">
-                <h1 className="font-title text-main-2 text-6xl w-9/12 pt-20 pl-6"> WILL YOU'LL BE THE NEXT SPACE EXPLORER ?</h1>
-                <h2 className="font-title text-main-2 text-3xl pt-20 pb-10 pl-6">Next laucnh in </h2>
-                <Countdown />
-                <Button text="HOW IS YOUR LUCK ?" />
-                <h3 className="font-title text-main-2 text-sm pt-5 pl-6"> Live stream here</h3>
-            </div>
-    )
-}
+  return (
+    <div className="h-screen bg-cover bg-[url('../assets/images/goldenTicketBg.png')] text-main-2">
+      <h1 className="font-title text-main-2 text-6xl w-9/12 pt-20 pl-6">
+        {" "}
+        WILL YOU'LL BE THE NEXT SPACE EXPLORER ?
+      </h1>
+      <h2 className="font-title text-main-2 text-3xl pt-20 pb-10 pl-6">
+        Next laucnh in{" "}
+      </h2>
+      <Countdown />
+      <Button text="HOW IS YOUR LUCK ?" />
+      <h3 className="font-title text-main-2 text-sm pt-5 pl-6">
+        {" "}
+        Live stream here
+      </h3>
+    </div>
+  );
+};
 
-export default GoldenTicket ;
+export default GoldenTicket;
