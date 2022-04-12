f72b7b54a5a47e29ae8f2e5371a6554c34c9bc47
DAMIEN BIENVENU <damien.bienvenu@gmail.com>
2022-04-12 16:49:05 +0200

    creation of PreHome component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import GoldenTicket from "./components/GoldenTicket";
 import NavBar from "./components/NavBar";
+import PreHome from "./components/PreHome";
 
 function App() {
   return (
     <div className="App h-screen w-screen">
       <NavBar />
+      <PreHome />
       <GoldenTicket />
     </div>
   );
---
+++ src/components/PreHome.jsx
@@ -0,0 +1,11 @@
+import Button from "./Button"
+
+const Prehome = () => {
+    return (
+        <div className="h-screen bg-left-bottom bg-black bg-[url('../assets/images/freelanceMainPic.png')]">
+            <Button text="Freelancer"/>
+            <Button text="Recruiter"/>
+        </div>
+    )
+}
+export default Prehome;
