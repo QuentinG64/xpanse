b9eb2687f3e76abda798253d2caf1e57e1df1b77
Nawel Benaicha <nawelbenaicha@MacBook-Pro-de-Nawel.local>
2022-04-14 13:57:06 +0200

    MAJ

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Video from "./Video.jsx";
 import BlockTxt from "./BlockTxt";
+import Infos from "./Infos";
 
 const AboutUs = () => {
   return (
     <div>
       <Video />
       <BlockTxt />
+      <Infos />
     </div>
   );
 };
