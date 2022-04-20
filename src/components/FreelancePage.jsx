3ed57f5da040a89626ac89967c547bedfda59ae3
DAMIEN BIENVENU <damien.bienvenu@gmail.com>
2022-04-20 16:10:42 +0200

    api called on freelancer page

--- src/components/FreelancePage.jsx
+++ src/components/FreelancePage.jsx
@@ -2,9 +2,26 @@ import React from "react";
 import CardFreelance from "./CardFreelance";
 import FilterFreelance from "./FilterFreelance";
 import NavBar from "./NavBar";
-import dataMission from "../data/dataMission";
+// import dataMission from "../data/dataMission";
+import axios from 'axios';
+import { useState, useEffect } from "react";
+
 
 const FreelancePage = () => {
+  const [missions, setMissions] = useState([]);
+
+  const getMissions = () => {
+    axios
+      .get(`https://ll.thespacedevs.com/2.2.0/launch/`)
+      .then((res) => res.data.results)
+      .then((data)=>{
+        setMissions(data);
+      })
+          .catch((err) => console.log(err));
+    };
+   useEffect(() => {
+    getMissions();
+   }, []);
   return (
     <div className="h-screen w-screen">
       <NavBar />
@@ -12,10 +29,11 @@ const FreelancePage = () => {
         <div className="absolute mt-32">
           <FilterFreelance />
         </div>
+         {/* {<button className="text-white z-50" onClick={() => { getMission() }}>get</button> } */}
         <div className="flex justify-center items-center ml-[30%] mr-[1%]">
           <div className="flex flex-wrap bg-transparent pt-24 pb-12 items-center gap-5">
-            {dataMission.map((data, index) => (
-              <CardFreelance key={index} {...data} />
+            {missions.map((mission, index) => (
+              <CardFreelance key={index} {...mission} />
             ))}
           </div>
         </div>
