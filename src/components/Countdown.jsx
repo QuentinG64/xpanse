7273cdc954914130ceab229ca785b51334160553
AntsoTech <antso-rakotoson@hotmail.fr>
2022-04-28 21:02:13 +0200

    first responsive version of goldenticket page

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -11,18 +11,30 @@ function Countdown() {
   }, []);
 
   return (
-    <div className="grid grid-cols-3 w-1/3 pl-0">
+    <div className="grid grid-cols-3 w-1/3 pl-0 sm:w-[95%]">
       <div className="flex-col border-r-2 h-24 align-middle">
-        <h1 className="font-title text-main-2 text-7xl pt-1 pl-7">5</h1>
-        <h3 className="font-title text-main-2 text-xl mb-14 pl-7">days</h3>
+        <h1 className="font-title text-main-2 text-7xl pt-1 pl-7 sm:text-5xl">
+          5
+        </h1>
+        <h3 className="font-title text-main-2 text-xl mb-14 pl-7 sm:text-xl">
+          days
+        </h3>
       </div>
       <div className="flex-col border-r-2 h-24 align-middle">
-        <h1 className="font-title text-main-2 text-7xl pt-1 pl-7">3</h1>
-        <h3 className="font-title text-main-2 text-xl mb-14 pl-7">hours</h3>
+        <h1 className="font-title text-main-2 text-7xl pt-1 pl-7 sm:text-5xl">
+          3
+        </h1>
+        <h3 className="font-title text-main-2 text-xl mb-14 pl-7 sm:text-xl">
+          hours
+        </h3>
       </div>
       <div className="flex-col h-24 align-middle">
-        <h1 className="font-title text-main-2 text-7xl pt-1 pl-7">{timer}</h1>
-        <h3 className="font-title text-main-2 text-xl mb-14 pl-7">minutes</h3>
+        <h1 className="font-title text-main-2 text-7xl pt-1 pl-7 sm:text-5xl">
+          {timer}
+        </h1>
+        <h3 className="font-title text-main-2 text-xl mb-14 pl-7 sm:text-xl">
+          minutes
+        </h3>
       </div>
     </div>
   );
--- src/components/GoldenTicket.jsx
+++ src/components/GoldenTicket.jsx
@@ -25,10 +25,10 @@ const GoldenTicket = () => {
     // INITAL PAGE GOLDEN TICKET -- INTRODUCTION
     <div className="h-screen bg-cover bg-goldenTicket text-main-2">
       <NavBar />
-      <h1 className="font-title text-main-2 text-6xl w-9/12 pt-32 pl-7">
+      <h1 className="font-title text-main-2 text-6xl w-9/12 pt-32 pb-4 pl-[3%] sm:text-3xl sm:w-screen sm:text-center sm:pr-[3%] sm:pt-[15%]">
         WILL YOU BE THE NEXT SPACE EXPLORER?
       </h1>
-      <h2 className="font-title text-main-2 text-3xl pt-20 pb-10 pl-7">
+      <h2 className="font-title text-main-2 text-3xl pt-[3%] pb-10 pl-[3%] sm:text-4xl sm:text-center sm:text-primary-1 sm:border-t-2 sm:w-4/6 sm:m-auto">
         Next launch in
       </h2>
       <Countdown />
@@ -37,7 +37,7 @@ const GoldenTicket = () => {
       <div
         role="button"
         tabIndex={0}
-        className="mt-10 pl-7"
+        className="mt-10 pl-[3%] sm:flex sm:justify-center sm:pl-0 sm:pt-10"
         onKeyDown={forTheWin}
         // ici modifier la valeur dans le ternaire de result pour définir la probabilité de gain
         onClick={forTheWin}
@@ -45,7 +45,7 @@ const GoldenTicket = () => {
         <Button text="HOW IS YOUR LUCK?" />
       </div>
 
-      <h3 className="font-title text-main-2 text-sm pt-5 pl-7">
+      <h3 className="font-title text-main-2 text-sm pt-5 pl-[3%] sm:text-center sm:pl-0 ">
         Live stream <span className="text-lg">here</span>
       </h3>
     </div>
