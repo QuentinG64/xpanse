c5144e6a02519da76a6e1462c3cf3e81877d5dab
AntsoTech <antso-rakotoson@hotmail.fr>
2022-04-30 20:54:51 +0200

    fixed prehome and screen bugs on freelance page and rerecruter page

--- src/components/FreelancePage.jsx
+++ src/components/FreelancePage.jsx
@@ -24,16 +24,17 @@ const FreelancePage = () => {
   return (
     <div className="h-screen w-screen">
       <NavBar />
-      <div className="flex flex-col bg-fixed bg-cover bg-freePage h-full w-full">
+      <div className="flex flex-col bg-fixed bg-cover bg-freePage">
         <div className="absolute mt-32">
           <FilterFreelance status={status} setStatus={setStatus} />
         </div>
         {/* {<button className="text-white z-50" onClick={() => { getMission() }}>get</button> } */}
         <div className="flex justify-center items-center ml-[30%] mr-[1%]">
           <div className="flex flex-wrap bg-transparent pt-24 pb-12 items-center gap-5">
-            {missions.map((mission, index) => (
-              <CardFreelance key={index} {...mission} />
-            ))}
+            {missions &&
+              missions.map((mission, index) => (
+                <CardFreelance key={index} {...mission} />
+              ))}
           </div>
         </div>
       </div>
